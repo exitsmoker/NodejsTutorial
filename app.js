4739460ec891ced401b59ecbdf35357f86d2cde9
jaylee <jaylee@jayleeui-MacBook-Pro.local>
2019-11-09 13:58:59 +0900

    app.js modifying

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    fs.readFile('index.html', (error, data)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);    
+    })
 });
 
 server.listen(port, hostname, ()=>{
